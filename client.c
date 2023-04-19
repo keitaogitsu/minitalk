3c4143bdc4c21835e9975d5374f3a5ada0a1632d
Keita Ogitsu <kogitsu@c1r9s7.42tokyo.jp>
2023-04-19 21:56:18 +0900

    修正

---
+++ client.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kogitsu <kogitsu@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/19 21:01:29 by kogitsu           #+#    #+#             */
+/*   Updated: 2023/04/19 21:55:46 by kogitsu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int		p_id;
+	char	str;
+
+	p_id = atoi(argv[1]);
+	str = argv[2];
+}
